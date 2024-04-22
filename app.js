c26f4cc8703b83b5687041577ce6f4271ead272c
vjeromel276 <vjeromel276@gmail.com>
2024-04-22 15:09:43 -0400

    update app.js to include home.html

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const http = require( 'http' );
+const fs = require( 'fs' );
 const PORT = 3000;
 
 http.createServer( function ( req, res ) {
 	res.writeHead( 200, { 'Content-Type': 'text/html' } );
 	//write imported file to page
-	req.url === '/about' ? res.write("<h1>My name is Vincent</h1>") : 
+	req.url === '/about' ? fs.createReadStream( 'home.html' ).pipe( res ): 
 	res.write( "<h1>On our way to being a full stack engineer</h1>" );
 	res.end();
 } ).listen( PORT );
