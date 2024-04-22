95e4f0b7e55223b7125e09ddc5b2c6668553d9d6
vjeromel276 <vjeromel276@gmail.com>
2024-04-22 14:33:35 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const PORT = 3000;
 http.createServer( function ( req, res ) {
 	res.writeHead( 200, { 'Content-Type': 'text/html' } );
 	//write imported file to page
-	req.url === '/about' ? res.write("<h1>On our way to being a full stack engineer</h1>") : 
+	req.url === '/about' ? res.write("<h1>My name is Vincent</h1>") : 
 	res.write( "<h1>On our way to being a full stack engineer</h1>" );
 	res.end();
 } ).listen( PORT );
