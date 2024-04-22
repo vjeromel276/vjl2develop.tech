3bf4378700c0bfb5d8eb5d4321db344618d2c34b
vjeromel276 <vjeromel276@gmail.com>
2024-04-22 14:29:42 -0400

    added html to pull into app.js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const http = require( 'http' );
 const PORT = 3000;
 
+const html = "./home.html"
+
 http.createServer( function ( req, res ) {
 	res.writeHead( 200, { 'Content-Type': 'text/html' } );
-	req.url === '/about' ? res.write( "<h1>Full Stack Engineer</hjson>" ) :
+	req.url === '/about' ? res.write( html ) :
 	res.write( "<h1>On our way to being a full stack engineer</h1>" );
 	res.end();
 } ).listen( PORT );
