6a2303a11cd60ad1e32d0c527bfca2a00ffe0da0
vjeromel276 <vjeromel276@gmail.com>
2024-04-27 11:45:06 -0400

    web socket server and client

--- index-ws.js
+++ index-ws.js
@@ -1,4 +1,5 @@
 const express = require( 'express' );
+const { WebSocketServer } = require( 'ws' );
 const server = require( 'http' ).createServer();
 
 const app = express();
@@ -10,4 +11,37 @@ app.get( '/', ( req, res ) => {
 server.on( 'request', app );
 server.listen( 3000, () => {
     console.log( 'Server started on http://localhost:3000' );
+} );
+
+/**
+ * Begin Web Socket
+ */
+
+const WebSocket = require( 'ws' ).server;
+
+const wss = new WebSocketServer( { server: server } );
+
+wss.broadcast = function broadcast ( data ) {
+    wss.clients.forEach( function each ( client ) {
+            client.send( data );
+    } );
+};
+
+wss.on( 'connection', function connection ( ws ) {
+    const numClients = wss.clients.size;
+    console.log( `Client connected. Total clients: ${ numClients }` );
+
+    wss.broadcast( `Current Visitors: ${ numClients }` );
+
+    if ( ws.readyState === ws.OPEN ) {
+        ws.send( 'Welcome to my server!' );
+    }
+
+    ws.on( 'close', function close () { 
+        wss.broadcast( `Current Visitors: ${ wss.clients.size }` );
+        console.log( `Client Disconnected, Current Visitors: ${ wss.clients.size }` );
+        
+    });
+
+
 } );
