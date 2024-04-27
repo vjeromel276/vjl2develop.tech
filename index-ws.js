95de5e87903ebc6ef078fb9352215a012bcd30ca
vjeromel276 <vjeromel276@gmail.com>
2024-04-27 14:40:03 -0400

    everything seems to be wokring

--- index-ws.js
+++ index-ws.js
@@ -17,13 +17,22 @@ server.listen( 3000, () => {
  * Begin Web Socket
  */
 
-const WebSocket = require( 'ws' ).server;
-
 const wss = new WebSocketServer( { server: server } );
+
+process.on( 'SIGINT', ( e ) => {
+    console.log( e );
+    wss.clients.forEach( function each ( client ) {
+        client.close();
+    } );
+    console.log( 'Shutting down server...' );
+    server.close( () => {
+        shutDownDB();
+    } );
+} );
 
 wss.broadcast = function broadcast ( data ) {
     wss.clients.forEach( function each ( client ) {
-            client.send( data );
+        client.send( data );
     } );
 };
 
@@ -37,11 +46,44 @@ wss.on( 'connection', function connection ( ws ) {
         ws.send( 'Welcome to my server!' );
     }
 
-    ws.on( 'close', function close () { 
+    db.run( `
+        INSERT INTO visitors (count, time) 
+        VALUES ( ${ numClients }, datetime('now'))
+    ` );
+
+    ws.on( 'close', function close () {
         wss.broadcast( `Current Visitors: ${ wss.clients.size }` );
         console.log( `Client Disconnected, Current Visitors: ${ wss.clients.size }` );
-        
-    });
 
-
+    } );
 } );
+
+/**
+ * End Web Socket
+ * Begin Database
+ */
+const sqlite = require( 'sqlite3' );
+const db = new sqlite.Database( ':memory:' );
+
+db.serialize( () => {
+    db.run( `
+        CREATE TABLE visitors (
+            count INTEGER,
+            time TEXT
+        )
+    `);
+} );
+
+function getCount () {
+    db.each( 'SELECT * FROM visitors', ( err, row ) => {
+        err ? console.error( err ) : console.log( row );
+    } );
+}
+
+function shutDownDB () {
+    getCount();
+    console.log( 'Shutting down db...' );
+    db.close( () => {
+        console.log( 'Database closed.' );
+    } );
+}
